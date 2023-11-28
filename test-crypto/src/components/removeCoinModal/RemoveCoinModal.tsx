15f668a6d01a4a2df0905a03b0d7e75a5cde3185
Irina Mazenkova <irinamazenkova2@gmail.com>
2023-11-28 21:59:16 +0300

    remove function

--- test-crypto/src/components/removeCoinModal/RemoveCoinModal.tsx
+++ test-crypto/src/components/removeCoinModal/RemoveCoinModal.tsx
@@ -1,9 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import BackpackCoinsContext, { ICoin } from '../../context/backpackCoinContext';
 import styles from './RemoveCoinModal.module.scss';
-
+import { roundingNumericValues } from '../../utils/formatNumericValue';
 interface RemoveCoinModalProps {
-
     isOpen: boolean;
     onClose: () => void;
 }
@@ -14,11 +13,11 @@ const RemoveCoinModal: React.FC<RemoveCoinModalProps> = ({
 }) => {
     const context = useContext(BackpackCoinsContext)
     const { getBackpack } = context!;
-    const [backpack, setBackpack] = useState<ICoin[]>();
+    const [backpackCoins, setBackpackCoins] = useState<ICoin[]>();
 
     useEffect(() => {
         const data = getBackpack()
-        setBackpack(data)
+        setBackpackCoins(data)
     }, [])
 
 
@@ -30,7 +29,9 @@ const RemoveCoinModal: React.FC<RemoveCoinModalProps> = ({
 
     const handleRemove = (coinId: string) => {
         try {
-
+            const newBackpack = backpackCoins?.filter(item => item.coinId !== coinId)
+            setBackpackCoins(newBackpack)
+            localStorage.setItem("backpack", JSON.stringify(newBackpack))
         } catch (error) {
             console.log("UpdateCoinQuantity Error: ", error)
         }
@@ -47,14 +48,17 @@ const RemoveCoinModal: React.FC<RemoveCoinModalProps> = ({
                 </div>
                 <div className={styles.coin_list_container}>
                     <ul className={styles.coin_list}>
-                        {backpack && backpack.map((coin) => (
+                        {backpackCoins && backpackCoins.map((coin) => (
                             <li key={coin.coinId} className={styles.coin_item}>
-                                <div>{`Coin ID: ${coin.coinId}`}</div>
-                                <div>{`Quantity: ${coin.quantity}`}</div>
-                                <div>{`Cost: ${coin.cost}`}</div>
-                                <div>{`Total: ${coin.quantity * coin.cost}`}</div>
-                                <button onClick={() => handleRemove(coin.coinId)}>Удалить</button>
+                                <div><div>{`Coin ID: ${coin.coinId}`}</div>
+                                    <div>{`Quantity: ${coin.quantity}`}</div>
+                                    <div>{`Cost: ${roundingNumericValues(coin.cost)}`}</div>
+                                    <div>{`Total: ${roundingNumericValues(coin.quantity * coin.cost)}`}</div>
+                                </div>
+
+                                <button onClick={() => handleRemove(coin.coinId)}>Remove</button>
                             </li>
+
                         ))}
                     </ul>
                 </div>
