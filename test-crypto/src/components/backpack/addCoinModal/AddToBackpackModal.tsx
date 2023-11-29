3636f5da57de6be7833389ab9df276a70e5701ae
Irina Mazenkova <irinamazenkova2@gmail.com>
2023-11-29 19:24:24 +0300

    change AddToBackpack style

--- test-crypto/src/components/backpack/addCoinModal/AddToBackpackModal.tsx
+++ test-crypto/src/components/backpack/addCoinModal/AddToBackpackModal.tsx
@@ -17,49 +17,57 @@ const AddToBackpack: React.FC<AddToBackpackProps> = ({
     isOpen,
     closeModal,
 }) => {
-    const context = useContext(BackpackCoinsContext)
+    const context = useContext(BackpackCoinsContext);
     const { setBackpack } = context!;
 
-    const [quantity, setQuantity] = useState<number>();
+    const [quantity, setQuantity] = useState<number | undefined>();
     const [showEmptyInput, setShowEmptyInput] = useState(false);
     const [showSuccNotification, setShowSuccNotification] = useState(false);
 
-
     const handleAdd = () => {
         try {
-            if (!quantity || quantity < 0 || quantity > 1000001) return setShowEmptyInput(true);
+            if (!quantity || quantity < 0 || quantity > 1000001) {
+                return setShowEmptyInput(true);
+            }
             setBackpack(coinId, quantity, cost);
-            setShowSuccNotification(true)
+            setShowSuccNotification(true);
         } catch (error) {
-            console.log("UpdateCoinQuantity Error: ", error)
+            console.log("UpdateCoinQuantity Error: ", error);
         }
     };
 
     return (
         <Modal isOpen={isOpen} onClose={closeModal}>
-
             {showSuccNotification ? (
-         
                 <div className={styles.notification}>
-                    <p> ✓ Coins added to backpack!</p>
+                    <p>✓ Coins added to backpack!</p>
                 </div>
-            ) : (<>
-                <h2>Add to Backpack</h2>
-                <p>Coin: {coinId}</p>
-                <p>Cost: {roundingNumericValues(cost)}</p>
-                <div className={styles.quintity_container}>
-                    <input type='number' min={0} max="1000000" value={quantity} onChange={(e) => {
-                        setQuantity(parseInt(e.target.value))
-                    }} />
+            ) : (
+                <div>
+                    <h2>Add to Backpack</h2>
+                    <p>Coin: {coinId}</p>
+                    <p>Cost: {roundingNumericValues(cost)}</p>
+                    <div className={styles.quantity_container}>
+                        <input
+                            type="number"
+                            min={0}
+                            max="1000000"
+                            value={quantity}
+                            onChange={(e) => {
+                                setQuantity(parseInt(e.target.value));
+                            }}
+                        />
+                    </div>
+                    {showEmptyInput && (
+                        <p className={styles.empty_input}>
+                            Enter the correct quantity (between 0 and 1000000)!
+                        </p>
+                    )}
+                    <div className={styles.button_add_container}>
+                        <button onClick={handleAdd}>Add</button>
+                    </div>
                 </div>
-
-                {showEmptyInput && (<p className={styles.emptyInput}>Enter the correct quantity (between 0 and 1000000)!</p>)}
-                <div className={styles.button_add_container}>
-                    <button onClick={handleAdd}>Add</button>
-
-                </div>
-
-            </>)}
+            )}
         </Modal>
     );
 };
